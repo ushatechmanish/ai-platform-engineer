5d56e60f04f98e6c38b9a8b73b04a4e6d1f0283f
Manish Chauhan <manish.fastprep@gmai.com>
2026-05-09 23:55:59 +0530

    Update to use native structured output closes #18

--- spring-ai-documentation/src/main/java/in/ushatech/springaidocumentation/entity/QuestionResponse.java
+++ spring-ai-documentation/src/main/java/in/ushatech/springaidocumentation/entity/QuestionResponse.java
@@ -2,6 +2,7 @@ package in.ushatech.springaidocumentation.entity;
 
 import java.util.List;
 
+// Dummy commit to close Issue 18
 public record QuestionResponse(
         List<Question> questions
 ) {
